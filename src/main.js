07a18d623b161da9a17befaf33ced6e147b1fafd
reona5 <shimada.reona@gmail.com>
2019-08-24 18:20:20 +0900

    choir:Firebaseの導入

--- src/main.js
+++ src/main.js
@@ -4,8 +4,21 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqLSoYUQ6uGPT4pv4THpufT1D2zTd_0Rk",
+  authDomain: "address-book-app-88895.firebaseapp.com",
+  databaseURL: "https://address-book-app-88895.firebaseio.com",
+  projectId: "address-book-app-88895",
+  storageBucket: "",
+  messagingSenderId: "1073269836092",
+  appId: "1:1073269836092:web:bdc00d733e4af877"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
